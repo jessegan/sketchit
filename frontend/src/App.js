c8eb38443636d6c5521cc9cd76279fb3b69a8504
jessegan <jesse13579@gmail.com>
2020-10-12 13:56:44 -0400

    Add react routing and root path to Home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header'
+import Home from './containers/Home'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = props => {
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={ Home } />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
