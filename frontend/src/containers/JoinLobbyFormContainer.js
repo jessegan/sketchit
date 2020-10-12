01c0ccbbe87b529c6a0d8d91f343ed05a3b203ac
jessegan <jesse13579@gmail.com>
2020-10-12 15:18:02 -0400

    Create JoinLobbyFormContainer

---
+++ frontend/src/containers/JoinLobbyFormContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import JoinLobbyForm from '../components/JoinLobbyForm'
+
+export class JoinLobbyFormContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        <JoinLobbyForm />
+      </div>
+    )
+  }
+}
+
+export default JoinLobbyFormContainer
