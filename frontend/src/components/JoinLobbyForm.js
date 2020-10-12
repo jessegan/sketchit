be7def8c9beaa82af7fabfe86099e91ebc7c46db
jessegan <jesse13579@gmail.com>
2020-10-12 15:21:17 -0400

    Add label and placeholders to form

--- frontend/src/components/JoinLobbyForm.js
+++ frontend/src/components/JoinLobbyForm.js
@@ -3,8 +3,14 @@ import React from 'react'
 const JoinLobbyForm = ({ submit }) => {
   return (
     <form onSubmit={ submit }>
-      <input type='text' name='name' />
+      Player Name
+      <br></br>
+      <input type='text' name='name' placeholder='Enter Name' />
+      <br></br>
+      Lobby Code
+      <br></br>
       <input type='text' name='code' placeholder='Enter Code' />
+      <br></br>
       <input type='submit' value='Join' />
     </form>
   )
