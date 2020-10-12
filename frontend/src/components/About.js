e5f10c30d40286bd425216012dd8f6240aded2b2
jessegan <jesse13579@gmail.com>
2020-10-12 14:28:54 -0400

    Create About section component

---
+++ frontend/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = (props) => {
+  return (
+    <div>
+      <p>Sketchit is the craziest game of pictionary you will ever play!</p>
+    </div>
+  )
+}
+
+export default About
