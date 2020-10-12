1a396cb961aab4c1556b90034483493bf7829519
jessegan <jesse13579@gmail.com>
2020-10-12 14:48:44 -0400

    Add buttons to set which homeForm to render

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react'
 import About from '../components/About'
 
 export class Home extends Component {
+
+  state={
+    homeForm: 0
+  }
+
+  handleOpenFormButton = (option) => {
+    this.setState({
+      homeForm: option
+    })
+  }
+
   render() {
     return (
       <div>
+        <button onClick={()=>this.handleOpenFormButton(1)}>Create Lobby</button>
+        <button onClick={()=>this.handleOpenFormButton(2)}>Join Lobby</button>
         <About />
       </div>
     )
