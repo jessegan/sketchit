39f38b65c485b95a80cb53df8c58f8d8208fc9f6
jessegan <jesse13579@gmail.com>
2020-10-12 16:06:49 -0400

    Add join route to lobby member

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   resources :lobbies, only: [:show,:create,:destroy], defaults: {format: 'json'} do
+    member do
+      post '/join', to: 'lobbies#join'
+    end
   end
 
 end
