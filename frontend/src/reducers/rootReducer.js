8192c68ab4cfdf77824623e46539fcde6f3d646f
jessegan <jesse13579@gmail.com>
2020-10-12 16:47:27 -0400

    Create rootReducer and add handlers for joining lobby actions

---
+++ frontend/src/reducers/rootReducer.js
@@ -0,0 +1,31 @@
+function rootReducer(state={
+  player: {},
+  lobby: {},
+  joining: false
+}, action){
+
+  switch(action.type){
+
+    case("START_JOIN_LOBBY"):
+      return {
+        player: {},
+        lobby: {},
+        joining: true
+      }
+    case("JOIN_LOBBY"):
+      return {
+        player: {...action.player},
+        lobby: {...action.lobby},
+        joining: false
+      }
+    case("FAILED_JOIN_LOBBY"):
+      return {
+        player: {},
+        lobby: {},
+        joining: false
+      }
+
+    default:
+      return state
+  }
+}
