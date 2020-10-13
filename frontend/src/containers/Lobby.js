7a8d07f11a9c569154b7c9b38a50a570e481429c
jessegan <jesse13579@gmail.com>
2020-10-13 13:11:08 -0400

    Add code to lobby component render

--- frontend/src/containers/Lobby.js
+++ frontend/src/containers/Lobby.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 export class Lobby extends Component {
   render() {
     return (
       <div>
-        
+        { this.props.lobby.code }
       </div>
     )
   }
 }
 
-export default Lobby
+function mapStateToProps(state){
+  return {
+    lobby: state.lobby
+  }
+}
+
+export default connect(mapStateToProps)(Lobby)
