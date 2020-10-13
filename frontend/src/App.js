5463f370d46d74cd1f74945d1a57e0bd6a6a935e
jessegan <jesse13579@gmail.com>
2020-10-13 16:16:57 -0400

    Convert App to class component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Header from './components/Header'
@@ -7,18 +7,21 @@ import Lobby from './containers/Lobby'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-const App = props => {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path="/" component={ Home } />
-          <Route exact path="/:id" render={routerProps=> <Lobby {...routerProps} />} />
-        </Switch>
-      </div>
-    </Router>
-  )
+class App extends Component {
+
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route exact path="/:id" render={routerProps=> <Lobby {...routerProps} />} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  } 
 }
 
 export default App;
