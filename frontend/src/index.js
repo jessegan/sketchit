fc16b2a97834d1222e34039f8ecb84895374ee07
jessegan <jesse13579@gmail.com>
2020-10-13 08:34:06 -0400

    Add redux and thunk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore,applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/rootReducer'
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer,applyMiddleware(thunk))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  ,
   document.getElementById('root')
 );
