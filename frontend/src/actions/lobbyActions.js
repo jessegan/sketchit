7a0cc89a372e87de38a38b88eea0d1183e259b0d
jessegan <jesse13579@gmail.com>
2020-10-13 13:10:05 -0400

    Add post request to api to join lobby

--- frontend/src/actions/lobbyActions.js
+++ frontend/src/actions/lobbyActions.js
@@ -1,5 +1,25 @@
-export const joinLobby = () => {
+const BASEURL = "http://localhost:8000"
+
+export const joinLobby = ({ name,code }) => {
   return (dispatch) => {
     dispatch({type: "START_JOIN_LOBBY"})
+
+    let config = {
+      method: "POST",
+      headers: {
+        ContentType: 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({ name: name })
+    }
+
+    fetch(`${BASEURL}/lobbies/${code}/join`,config)
+      .then(resp => resp.json())
+      .then(data => { console.log(data)
+        dispatch({
+          type: "JOIN_LOBBY",
+          ...data
+        })
+      })
   }
 }
