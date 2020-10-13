64812c31382019d51136f4fd470efd07338ca01a
jessegan <jesse13579@gmail.com>
2020-10-13 16:08:01 -0400

    Render players on lobby container

--- frontend/src/containers/Lobby.js
+++ frontend/src/containers/Lobby.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 export class Lobby extends Component {
+
+  genPlayers = () => {
+    return this.props.lobby.players.map( (p,i) => {
+      return <h3 key={i}>{p.name}</h3>
+    })
+  }
+
   render() {
     return (
       <div>
         { this.props.lobby.code }
+        { this.genPlayers() }
       </div>
     )
   }
