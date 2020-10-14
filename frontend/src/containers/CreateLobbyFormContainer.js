d863ef8d24983fb5cde9fa9b918083cd17c405ff
jessegan <jesse13579@gmail.com>
2020-10-14 09:23:18 -0400

    Add CreateLobbyForm component with form fields passed in

--- frontend/src/containers/CreateLobbyFormContainer.js
+++ frontend/src/containers/CreateLobbyFormContainer.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
+import CreateLobbyForm from '../components/CreateLobbyForm'
+
 export class CreateLobbyFormContainer extends Component {
+
+  state = {
+    fields: {
+      name: ""
+    },
+    errors: {}
+  }
+
   render() {
     return (
       <div>
-        
+        <CreateLobbyForm fields={ this.state.fields } />
       </div>
     )
   }
