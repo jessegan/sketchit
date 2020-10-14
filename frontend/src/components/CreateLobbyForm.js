58488609f0be31f6764c33f2923dcaa1dfcb005e
jessegan <jesse13579@gmail.com>
2020-10-14 09:33:20 -0400

    Add onChange handler

--- frontend/src/components/CreateLobbyForm.js
+++ frontend/src/components/CreateLobbyForm.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const CreateLobbyForm = ({ fields }) => {
+const CreateLobbyForm = ({ fields, onChange }) => {
   return (
     <form>
       Player Name
       <br></br>
-      <input type='text' name='name' placeholder='Enter Name' value={ fields.name }  />
+      <input type='text' name='name' placeholder='Enter Name' value={ fields.name } onChange={ onChange } />
       <br></br>
       <input type='submit' value='Create' />
     </form>
