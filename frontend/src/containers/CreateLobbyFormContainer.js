6d9d7572b2b1715cbdc91579cdee22875e627568
jessegan <jesse13579@gmail.com>
2020-10-14 09:33:02 -0400

    Add handleOnChange method and pass it down to CreateLobbyForm

--- frontend/src/containers/CreateLobbyFormContainer.js
+++ frontend/src/containers/CreateLobbyFormContainer.js
@@ -11,10 +11,27 @@ export class CreateLobbyFormContainer extends Component {
     errors: {}
   }
 
+  handleOnChange = e => {
+    let newVal = e.target.value
+
+    if(e.target.name === "name"){
+      if(newVal.length > 20){
+        return
+      }
+    }
+
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        [e.target.name]: newVal
+      } 
+    })
+  }
+
   render() {
     return (
       <div>
-        <CreateLobbyForm fields={ this.state.fields } />
+        <CreateLobbyForm fields={ this.state.fields } onChange={ this.handleOnChange } />
       </div>
     )
   }
