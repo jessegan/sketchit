e68a02965f407ee28fca823c8513daa39673d8ff
jessegan <jesse13579@gmail.com>
2020-10-15 11:41:56 -0400

    Delete received method

--- backend/app/channels/lobby_channel.rb
+++ backend/app/channels/lobby_channel.rb
@@ -1,15 +1,9 @@
 class LobbyChannel < ApplicationCable::Channel
   def subscribed
     @lobby = Lobby.find_by(code: params[:code])
-    puts "subscribed to lobby"
     stream_for @lobby
   end
 
-  # def received(data)
-  #   LobbyChannel.broadcast_to(@lobby, { players: @lobby.players })
-  # end
-
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
   end
 end
