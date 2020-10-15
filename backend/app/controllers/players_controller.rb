9e612a273edcdcffe0797258db56909208b25099
jessegan <jesse13579@gmail.com>
2020-10-15 11:40:04 -0400

    Init Players controller and add create action

---
+++ backend/app/controllers/players_controller.rb
@@ -0,0 +1,20 @@
+class PlayersController < ApplicationController
+
+  def create
+    @lobby = Lobby.find_by(code: params[:code])
+
+    if @lobby
+      @player = Player.create(name: params[:name], lobby: @lobby)
+
+      LobbyChannel.broadcast_to(@lobby,{ players: @lobby.players })
+
+      render :create
+    else
+      render json: {
+        error: "Unable to find lobby",
+        status: 400
+      }, status: 400
+    end
+  end
+
+end
