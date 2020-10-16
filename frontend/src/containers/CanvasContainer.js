5ed0dc6ba7310d62e97a8bd09df2eedab1c701bd
jessegan <jesse13579@gmail.com>
2020-10-16 10:32:14 -0400

    Subscribe to canvas channel

--- frontend/src/containers/CanvasContainer.js
+++ frontend/src/containers/CanvasContainer.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { CableApp } from '../index'
 
 import Canvas from '../components/Canvas'
 
 export class CanvasContainer extends Component {
+
+  componentDidMount(){
+    // Subscribe to canvas channel
+    CableApp.canvas = CableApp.cable.subscriptions.create({
+      channel: "CanvasChannel",
+      lobby_code: this.props.lobbyCode
+    })
+
+  }
+
   render() {
     return (
       <div>
