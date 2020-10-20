a56fd6bf64765284ad6ad093c13b77386d0b9858
jessegan <jesse13579@gmail.com>
2020-10-20 11:40:03 -0400

    Style error message with bootstrap Alert

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Alert from 'react-bootstrap/Alert'
 
 const Header = props => {
   return (
     <>
-    { props.errors.length > 0 ? <span style={{color: "red"}}>{ props.errors[0]}</span> : <></> }
+    { props.errors.length > 0 ? <Alert variant="warning" >{ props.errors[0]}</Alert> : <></> }
     <h1>Sketchit</h1>
     </>
   )
