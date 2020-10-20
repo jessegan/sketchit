97348da8f9bea24ae62839d6000a2e05cae63156
jessegan <jesse13579@gmail.com>
2020-10-20 15:28:41 -0400

    Remove div

--- frontend/src/components/PlayersList.js
+++ frontend/src/components/PlayersList.js
@@ -3,12 +3,12 @@ import React from 'react'
 const PlayersList = ({ players }) => {
   let playersList = players.map( (p) => (<p key={ p.id }>{ p.name }</p>))
   return (
-    <div>
+    <>
       <h3>Players</h3>
 
       { playersList}
 
-    </div>
+    </>
   )
 }
 
