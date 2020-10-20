d7f515f210514a7cd9462d8dc75c7d466131cd8a
jessegan <jesse13579@gmail.com>
2020-10-20 15:27:59 -0400

    Add button to copy lobby link

--- frontend/src/containers/Lobby.js
+++ frontend/src/containers/Lobby.js
@@ -5,6 +5,8 @@ import PlayersList from '../components/PlayersList'
 import { leaveLobby } from '../actions/lobbyActions'
 import { CanvasContainer } from './CanvasContainer'
 
+import Button from 'react-bootstrap/Button'
+
 export class Lobby extends Component {
 
   /**
@@ -24,12 +26,23 @@ export class Lobby extends Component {
     window.addEventListener("beforeunload", this.onUnload)
   }
 
+  copyLink = () => {
+    const el = document.createElement('textarea')
+    el.value = `localhost:3000/${this.props.lobby.code}`
+    document.body.appendChild(el)
+    el.select() 
+    document.execCommand("copy")
+    document.body.removeChild(el)
+  }
+
   render() {
     return (
       <div>
-        { this.props.lobby.code }
-        <PlayersList players={ this.props.lobby.players } />
+        <h3>{ this.props.lobby.code }</h3>
+        <Button variant="outline-secondary" size="sm" onClick={this.copyLink} style={{margin: "10px"}}>Copy Lobby Link</Button>
+
         <CanvasContainer lobbyCode={ this.props.lobby.code } />
+        <PlayersList players={ this.props.lobby.players } />
       </div>
     )
   }
