01be75a2b3fc6785e86fd4cba3565fa738f1cd82
jessegan <jesse13579@gmail.com>
2020-10-20 13:42:24 -0400

    Update About description

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const About = (props) => {
   return (
-    <div>
-      <p>Sketchit is the craziest game of pictionary you will ever play!</p>
+    <div style={{margin: "20px 0px"}}>
+      <h3>Welcome to Sketchit!</h3>
+      <p>Create and join lobbies with your friends and draw together on a collaborative sketch pad.</p>
+
+      <p><strong>Incoming Features:</strong> Saving sketches, Lobby Passwords</p>
     </div>
   )
 }
