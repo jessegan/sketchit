3043cf5071012ba80a92cf66da1f4bb24f6e53b0
jessegan <jesse13579@gmail.com>
2020-10-20 13:21:10 -0400

    Render logo in Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
+import logo from '../images/sketchit_logo.png'
+
 import Alert from 'react-bootstrap/Alert'
 
 const Header = props => {
   return (
     <>
     { props.errors.length > 0 ? <Alert variant="warning" >{ props.errors[0]}</Alert> : <></> }
-    <h1>Sketchit</h1>
+    <img src={ logo } height="100" />
     </>
   )
 }
